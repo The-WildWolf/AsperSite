10ccdd6e9ce2a5ba32d81dd374508560dbd42f56
The-WildWolf <ksenna.sitek@gmail.com>
2022-05-25 15:16:34 +0200

    commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,14 +74,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django-page',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
